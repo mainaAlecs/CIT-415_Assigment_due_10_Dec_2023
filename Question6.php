20c9e3a491b41bdc67d800865db770d814883d8e
AlecMaina <amlonwolf@gmail.com>
2023-12-10 19:02:36 +0300

    Code for Question 6

---
+++ Question6.php
@@ -0,0 +1,29 @@
+// Question 6 Implementation
+
+<?php
+
+function compress($chars)
+{
+    $result = '';
+    $count = 1;
+
+    for ($i = 0; $i < count($chars); $i++) {
+        // Check if the current character is the same as the next one
+        if ($i < count($chars) - 1 && $chars[$i] == $chars[$i + 1]) {
+            $count++;
+        } else {
+            // Append the result based on the count of repeating characters
+            $result .= ($count > 1) ? $chars[$i] . $count : $chars[$i];
+            $count = 1;
+        }
+    }
+
+    return $result;
+}
+
+// Example usage
+$charArray = ["a", "a", "b", "b", "c", "c", "c"];
+$compressedString = compress($charArray);
+
+echo "Compressed String: $compressedString";
+?>
